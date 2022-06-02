134b846b73ea12189554ad3913a96b4c9406f49e
alguitti <andre.guitti@yahoo.com.br>
2022-06-02 17:06:50 -0300

    DB initial instantiation

---
+++ src/main/java/com/andreguitti/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.andreguitti.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.andreguitti.domain.User;
+import com.andreguitti.repositories.UserRepository;
+
+//Classe para instanciar objetos para teste do projeto
+//Método run roda o método ao inicio do programa
+@Configuration 
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
